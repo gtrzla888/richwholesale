5da57a08625be431226cd4dff7d9f8fbcdad00f5
Lixing <zlxjackie@hotmail.com>
2019-04-28 20:11:23 +1000

    Update

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -10,8 +10,34 @@ use App\Http\Resources\Invoice as InvoiceResource;
 
 class InvoiceController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return InvoiceResource::collection(Invoice::all());
+        /** @var User $user */
+        $user = request()->user();
+
+        $query = Invoice::join('orders', 'orders.id', '=', 'invoices.order_id')->with('order.quote.company');
+
+        if ($company = $request->get('company')) {
+            $query->where('order.quote.company_id', $company);
+        }
+
+        if ($createdAt = $request->get('created_at')) {
+            $query->where('created_at', '>=', $createdAt);
+        }
+
+        return $query->get('invoices.*');
+    }
+
+    public function store(Request $request, Order $order)
+    {
+        $invoice = new Invoice();
+        $invoice->order_id = $order->id;
+        $invoice->save();
+
+    }
+
+    public function update(Request $request, Invoice $invoice) 
+    {
+
     }
 }
--- app/Invoice.php
+++ app/Invoice.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
+    const STATUS_PAID = 'Paid';
+
+    protected $guarded = [];
+
     public function order()
     {
         return $this->belongsTo(order::class);
